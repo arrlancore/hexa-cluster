4ddc95287ec1b67deca9b090993f3dc3685ff018
arrlancores <arrlancore@gmail.com>
2020-07-12 00:56:14 +0700

    Init cluster Add, get, remove

--- src/index.js
+++ src/index.js
@@ -61,20 +61,35 @@ function Cluster() {
 
     let isAllowed = false;
     const nodeTarget = this.get(id);
-    const childTarget = nodeTarget.borders;
+    if (!nodeTarget) {
+      console.error("target not found");
+      return false;
+    }
+    const childTarget = Object.values(nodeTarget.borders);
 
-    // remove child has neighbour
-    const nodeToCheck = Object.entries(childTarget);
+    if (childTarget.length === 1) {
+      isAllowed = true;
+    } else {
+      // remove child has neighbour
+      const nodeToCheck = handleRemoveChildNeighbour(childTarget);
+      const mappedNodes = mappingNode(nodeToCheck);
 
-    nodeToCheck.forEach(([border, property], i, arr) => {
-      console.log(arr);
-      const nodeChildName = arr.map(([b, { name }]) => name);
-      const childNode = Object.values(this.get(property.name).borders).map(
-        item => item.name
-      );
-      console.log(property.name);
-      console.log(nodeChildName, childNode);
-    });
+      const otherPathConnections = [];
+      mappedNodes.forEach(([from, to]) => {
+        // find another path connection
+        const result = findPathBFS(from, to, id);
+        if (result) {
+          otherPathConnections.push(true);
+        }
+      });
+      isAllowed = mappedNodes.length === otherPathConnections.length;
+    }
+    if (isAllowed) {
+      handleRemoveFromNodes(nodeTarget, id);
+    } else {
+      console.error("node is not allowed to be remove");
+    }
+    return isAllowed;
   };
 
   // private method
@@ -83,6 +98,133 @@ function Cluster() {
     borders: {},
     position: initialPosition
   });
+
+  const findPathBFS = (from, to, skippedNode) => {
+    const INCREMENT = 1; // increment cost
+    const initialNode = [null, from.name, 0]; // from, to, cost
+    // create queues
+    const queue = new Queue();
+    queue.enqueue(initialNode);
+    // create stack visited
+    const visited = new SimpleStack();
+
+    let result = null;
+
+    while (!queue.isEmpty() && !result) {
+      // get and delete from the queue
+      const x = queue.peek();
+      queue.dequeue();
+      // mark x as visited
+      visited.push(x);
+      // get successor
+      const excludesChildCheck = [...visited.getElements()].map(
+        item => item[1]
+      );
+      const skipped = [...excludesChildCheck, skippedNode];
+      const successorX = findChilds(x[1], skipped)
+        .map(item => [x[1], item.name, x[2] + INCREMENT])
+        .sort((a, b) => a[2] - b[2]);
+      // add successor to queue
+      successorX.forEach(queue.enqueue);
+      if (x[1] === to.name) {
+        result = visited.getElements();
+        queue.clearElements();
+      }
+    }
+    return result;
+  };
+
+  function Queue() {
+    let elements = [];
+
+    this.enqueue = e => elements.push(e);
+    this.dequeue = () => elements.shift();
+    this.peek = () => elements[0];
+    this.isEmpty = () => elements.length === 0;
+    this.getElements = () => elements;
+    this.clearElements = () => {
+      elements = [];
+    };
+  }
+
+  function SimpleStack() {
+    const elements = [];
+    const marker = [];
+    const INDEX_ELEMENT_NAME = 1;
+
+    this.push = element => {
+      if (!marker.includes(element[INDEX_ELEMENT_NAME])) {
+        marker.push(element[1]);
+        elements.push(element);
+      }
+    };
+    this.getElements = () => elements;
+  }
+
+  const findChilds = (nodeName, skipped = []) => {
+    const node = this.get(nodeName);
+    return node
+      ? Object.values(node.borders).filter(item => !skipped.includes(item.name))
+      : [];
+  };
+
+  const handleRemoveFromNodes = (node, key) => {
+    const nodeBorders = Object.entries(node.borders);
+    // delete from neighbour
+    nodeBorders.forEach(([key, property]) => {
+      const nodeNeighbour = this.get(property.name);
+      const borderTarget = bordersCounter[key][COUNTER_INDEX];
+      const newBorders = nodeNeighbour.borders;
+      delete newBorders[borderTarget];
+
+      // update borders
+      nodeNeighbour.borders = newBorders;
+      storage[property.name] = nodeNeighbour;
+    });
+    // delete on nodes
+    delete storage[key];
+  };
+
+  const handleRemoveChildNeighbour = list => {
+    const [startNode, ...rest] = list;
+    let checkedList = [];
+    rest.forEach(itemNode => {
+      if (
+        !isNeighboured(itemNode.name, startNode.name) &&
+        !isListNeighboured(checkedList, itemNode.name)
+      ) {
+        checkedList = [...checkedList, itemNode];
+      }
+    });
+    return [startNode, ...checkedList];
+  };
+
+  const isNeighboured = (firstNode, secondNode) => {
+    const node = this.get(firstNode);
+    const listNeighbour = Object.values(node.borders).map(each => each.name);
+    return listNeighbour.includes(secondNode);
+  };
+
+  const isListNeighboured = (list, nodeName) => {
+    const checkedList = list.map(item => {
+      return isNeighboured(item.name, nodeName);
+    });
+    return !!checkedList.length;
+  };
+
+  const mappingNode = list => {
+    let tempList = [...list];
+    const mappedNode = [];
+    for (let i = 0; i < list.length; i++) {
+      const start = tempList.shift();
+      if (tempList[0]) {
+        tempList.forEach(item => {
+          mappedNode.push([start, item]);
+        });
+      }
+    }
+    return mappedNode;
+  };
 
   const handleConnectNewNode = (newNode, targetNode) => {
     const neighboardNode = storage[targetNode.name];
@@ -131,7 +273,7 @@ function Cluster() {
     const toBorders = storage[toNode.name].borders;
     toBorders[toNode.border] = fromNode;
     storage[toNode.name].borders = toBorders;
-    console.log("success connected", 456);
+    console.log("success connected to", fromNode.name);
     return true;
   };
 
@@ -235,6 +377,9 @@ hexaland.add({ name: "i", border: 0 }, "h");
 hexaland.add({ name: "j", border: 2 }, "i");
 hexaland.add({ name: "k", border: 3 }, "j");
 hexaland.remove("c");
+hexaland.remove("d");
+hexaland.remove("i");
+hexaland.remove("b");
 
 const c3e = performance.now();
 console.log("case 3 running on ms:", (c3e - c3) / 1000);
